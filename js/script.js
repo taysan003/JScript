c7b8512c7f1613f09d42714da00de2e9a7c75b99
taysan003 <kostuchenko-and@mail.ru>
2015-12-28 20:35:18 +0300

    Result

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 $(function input_number(){
-
+ var arrayOperators = ["+", "-", "*", "/"];                      // To define array of operators.
    // Run if we pushed button with operator.                                                              
   $('.operator').click(function(){
   
     var calcInput = $('.calc-input');                           // Input.
     var lastValue = calcInput.val();                            // Save input. 
-    var lastChar = lastValue[lastValue.length-1];               // Take last character from input.
-    var arrayOperators = ["+", "-", "*", "/"];                  // To define array of operators.
+    var lastChar = lastValue[lastValue.length-2];               // Take last character from input.
+    
     if ($.inArray(lastChar, arrayOperators) != -1) {            // If last element of array is operator.
-        lastValue = lastValue.substring(0, lastValue.length-1); // Delete last element if isn't operator, save only numbers 
+        lastValue = lastValue.substring(0, lastValue.length-3); // Delete last element if isn't operator, save only numbers 
     }
     // Test first element (+5, -6). 
     if (lastValue != '0'){
-      calcInput.val(lastValue + $(this).val());                 //This - pushed element. Add to current element pushed button.
+      calcInput.val(lastValue + ' ' + $(this).val()+' ');       //This - pushed element. Add to current element pushed button.
            
     }
    
@@ -33,11 +33,46 @@ $(function input_number(){
     $('.clear').click(function(){
       $('.calc-input').val("0");  
   });
-  // Push button "=".
-  $('.result').click(function)(){
-  
-  
+  // Push button "="
+  $('.result').click(function(){
+  var calcInput = $('.calc-input');                           // Input.
+  var lastValue = calcInput.val();                            // Save input. 
+  var lastChar = lastValue[lastValue.length-2];               // Take last character from input.
+  if ($.inArray(lastChar, arrayOperators) != -1) {            // If last element of array is operator.
+      lastValue = lastValue.substring(0, lastValue.length-3); // Delete last element if isn't operator, save only numbers 
+          
+    }
+     calcInput.val(lastValue); 
+     var mass = lastValue.split(' ');
+     //console.log(mass);
+     var result = 0;
+     for(var i = 0; i < mass.length; i++ ){
+     
+       if ($.inArray(mass[i], arrayOperators) != -1){
+        switch (mass[i]) {
+          case '+':
+           result = parseInt( mass[i-1]) + parseInt(mass[i+1]);
+           mass[i+1] = result;
+           break;
+          case '-':
+           result = parseInt(mass[i-1]) - parseInt(mass[i+1]);
+           mass[i+1] = result;
+           break;
+          case '/':
+           result = parseInt(mass[i-1]) / parseInt(mass[i+1]);
+           mass[i+1] = result;
+           break;
+          case '*':
+           result = parseInt(mass[i-1]) * parseInt(mass[i+1]);
+           mass[i+1] = result;
+           break;
+          default:
+            
+        }
+      }
+    }
+
+  calcInput.val(result); 
   
   });
-  
 });
