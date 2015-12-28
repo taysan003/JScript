66b3f511f9c862a0fedc46b0300dc8df871992b0
taysan003 <kostuchenko-and@mail.ru>
2015-12-28 19:21:44 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
 $(function input_number(){
+
+   // Run if we pushed button with operator.                                                              
+  $('.operator').click(function(){
+  
+    var calcInput = $('.calc-input');                           // Input.
+    var lastValue = calcInput.val();                            // Save input. 
+    var lastChar = lastValue[lastValue.length-1];               // Take last character from input.
+    var arrayOperators = ["+", "-", "*", "/"];                  // To define array of operators.
+    if ($.inArray(lastChar, arrayOperators) != -1) {            // If last element of array is operator.
+        lastValue = lastValue.substring(0, lastValue.length-1); // Delete last element if isn't operator, save only numbers 
+    }
+    // Test first element (+5, -6). 
+    if (lastValue != '0'){
+      calcInput.val(lastValue + $(this).val());                 //This - pushed element. Add to current element pushed button.
+           
+    }
+   
+  });
+  // Run if was pushed button with number.
   $('.number').click(function(){
   
-    var calcInput = $('.calc-input');
-    var lastValue = calcInput.val(); 
+    var calcInput = $('.calc-input');                           // Input.
+    var lastValue = calcInput.val();                            // Save input.
+   // Test of true display input element without 0 at first (free space).
     if (lastValue == '0'){
       lastValue = '';
     }
-    calcInput.val(lastValue + $(this).val());
+    calcInput.val(lastValue + $(this).val());                    //This - pushed element. Add to current element pushed button.
 
-    
-    
   });
-  
+  // Push button "C".
     $('.clear').click(function(){
       $('.calc-input').val("0");  
+  });
+  // Push button "=".
+  $('.result').click(function)(){
+  
+  
+  
   });
   
 });
